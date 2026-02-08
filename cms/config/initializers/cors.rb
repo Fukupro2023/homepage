4ef522a2c2e4310f10edda383bcd01a37e2a6c03
蓮華丸 <167959774+Rengemaru@users.noreply.github.com>
2026-02-09 01:03:11 +0900

    update: URLを環境変数に対応

--- cms/config/initializers/cors.rb
+++ cms/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:4321"
+    origins ENV.fetch("CORS_ORIGIN", "localhost:4321")
 
     resource "/api/*",
       headers: :any,
