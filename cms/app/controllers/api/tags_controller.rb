821163d3e2350115a90f233f192cfb8ed4cde09d
itojun-1230 <itojum1230@gmail.com>
2026-02-11 16:44:39 +0900

    fix: 集計クエリでN+1問題回避

--- cms/app/controllers/api/tags_controller.rb
+++ cms/app/controllers/api/tags_controller.rb
@@ -1,15 +1,19 @@
 module Api
   class TagsController < ActionController::API
     def index
-      tags_with_count = Tag.all.map do |tag|
+      tags_with_count = Tag
+                        .left_joins(:blogs)
+                        .group("tags.id", "tags.name")
+                        .select("tags.id, tags.name, COUNT(blogs.id) AS blogs_count")
+                        .order("blogs_count DESC")
+
+      sorted_tags = tags_with_count.map do |tag|
         {
           id: tag.id,
           name: tag.name,
-          count: tag.blogs.count
+          count: tag.blogs_count.to_i
         }
       end
-
-      sorted_tags = tags_with_count.sort_by { |tag| -tag[:count] }
 
       render json: {
         tags: sorted_tags,
