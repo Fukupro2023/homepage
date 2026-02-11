fe9ea334aae6dbf7d173cba938e84e7ed758dc08
itojun-1230 <itojum1230@gmail.com>
2026-02-11 15:55:17 +0900

    feat: タグ一覧APIを実装

---
+++ cms/app/controllers/api/tags_controller.rb
@@ -0,0 +1,19 @@
+module Api
+  class TagsController < ActionController::API
+    def index
+      tags_with_count = Tag.all.map do |tag|
+        {
+          name: tag.name,
+          count: tag.blogs.count
+        }
+      end
+
+      sorted_tags = tags_with_count.sort_by { |tag| -tag[:count] }
+
+      render json: { 
+        tags: sorted_tags,
+        count: sorted_tags.count
+      }
+    end
+  end
+end
