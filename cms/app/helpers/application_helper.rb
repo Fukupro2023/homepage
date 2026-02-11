d77f4cd1ff79d5a99302b81738be9f504f589ab3
蓮華丸 <167959774+Rengemaru@users.noreply.github.com>
2026-02-11 16:24:17 +0900

    update: markdownに対応

--- cms/app/helpers/application_helper.rb
+++ cms/app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def markdown(text)
+    return "" if text.blank?
+
+    renderer = Redcarpet::Render::HTML.new(
+      hard_wrap: true,
+      filter_html: false
+    )
+    md = Redcarpet::Markdown.new(renderer,
+      autolink: true,
+      fenced_code_blocks: true,
+      tables: true,
+      strikethrough: true,
+      no_intra_emphasis: true)
+    md.render(text).html_safe # rubocop:disable Rails/OutputSafety
+  end
 end
