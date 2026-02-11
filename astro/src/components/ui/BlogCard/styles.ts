95f9a2231aa2946cd6eb7880868aa4f8e83549f9
itojun-1230 <itojum1230@gmail.com>
2026-02-11 23:16:43 +0900

    style: fix lint

--- astro/src/components/ui/BlogCard/styles.ts
+++ astro/src/components/ui/BlogCard/styles.ts
@@ -1,5 +1,6 @@
 export const blogCardStyles = {
-	container: "flex flex-row-reverse justify-between rounded-md border bg-white p-4 shadow-md sm:block",
+	container:
+		"flex flex-row-reverse justify-between rounded-md border bg-white p-4 shadow-md sm:block",
 	thumbnail: "h-full w-40 sm:h-40 sm:w-80 flex-shrink-0 object-cover ",
 	title: "line-clamp-3 font-bold text-gray-800 text-lg hover:underline",
 	publishedAt: "text-gray-500 text-sm",
