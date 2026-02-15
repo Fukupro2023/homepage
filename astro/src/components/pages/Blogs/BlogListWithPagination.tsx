79877c83e566ee08d1206451827ccd929a97c0e5
itojun-1230 <itojum1230@gmail.com>
2026-02-16 01:04:00 +0900

    fix: 記事一覧のレイアウトを最新記事と合わせる

--- astro/src/components/pages/Blogs/BlogListWithPagination.tsx
+++ astro/src/components/pages/Blogs/BlogListWithPagination.tsx
@@ -27,7 +27,7 @@ export default function BlogListWithPagination({
 			) : blogs.length === 0 ? (
 				<NoDataView />
 			) : (
-				<div className="grid grid-cols-1 gap-4 py-3 md:grid-cols-3">
+				<div className="grid sm:grid-cols-3 grid-cols-1 gap-4">
 					{blogs.map((blog: BlogItem) => (
 						<BlogCard key={blog.id} blog={blog} />
 					))}
