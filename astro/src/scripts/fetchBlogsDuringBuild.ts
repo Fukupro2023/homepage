2110a876e1fefc57d7fbc69d038fa68d64b91816
itojun-1230 <itojum1230@gmail.com>
2026-02-15 12:58:55 +0900

    fix: blog idの型を修正

--- astro/src/scripts/fetchBlogsDuringBuild.ts
+++ astro/src/scripts/fetchBlogsDuringBuild.ts
@@ -5,7 +5,7 @@ import { fileURLToPath } from "node:url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 interface Blog {
-	id: number;
+	id: string;
 	title: string;
 	description: string;
 	content: string;
